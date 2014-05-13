ae5e76eeeac972bcdaae593f7950205c5a520424
thanhhx <thanhhx@PersonalVPS.(none)>
2014-05-12 22:21:54 -0400

    add install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+if [ ! -d "$HOME/linux_workspace" ]; then
+  echo "Cloning from GitHub"
+  git clone https://github.com/vodkavn/linux_workspace.git
+fi
+
+cd "$HOME/linux_workspace"
+
+echo "Configuring vim setting..."
+cp -rf .vim ~/
+cp -f .vimrc ~/
+
+echo "Configuring bash setting..."
+cp -rf .bash ~/
+cp -f .bashrc ~/
+cp -f .bash_logout ~/
+cp -f .bash_profile ~/
+
+echo "Configuring git setting..."
+cp -f .gitconfig ~/
+
+echo "DONE!"
