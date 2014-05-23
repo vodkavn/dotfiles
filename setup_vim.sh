ee69ed730aa57e0ee37f07e35e75fcf75ac16b69
vodkavn <hoangxuanthanh1986@gmail.com>
2014-05-23 07:39:30 +0700

    Create setup_vim.sh

---
+++ setup_vim.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#---------------------------
+# Compile vim 7.4 from source code
+#
+yum -y remove $(rpm -qa | grep ^vim)
+yum -y groupinstall 'Development tools'
+yum -y install ncurses ncurses-devel
+cd /usr/local/src
+wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
+tar jxvf vim-*.tar.bz2
+rm -f vim-*.tar.bz2
+cd vim*
+./configure --prefix=/usr --with-features=huge --enable-multibyte
+make
+make install
+
+
+#---------------------------
