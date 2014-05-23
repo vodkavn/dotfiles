c2ba88ba03d58d773ce261a54ac38770d9340a77
vodkavn <hoangxuanthanh1986@gmail.com>
2014-05-23 07:46:39 +0700

    Update setup_vim.sh

--- setup_vim.sh
+++ setup_vim.sh
@@ -2,9 +2,12 @@
 #---------------------------
 # Compile vim 7.4 from source code
 #
-yum -y remove $(rpm -qa | grep ^vim)
-yum -y groupinstall 'Development tools'
-yum -y install ncurses ncurses-devel
+# yum -y remove $(rpm -qa | grep ^vim)
+# yum -y groupinstall 'Development tools'
+# yum -y install ncurses ncurses-devel
+
+yum -y install gcc make ncurses-devel
+
 cd /usr/local/src
 wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
 tar jxvf vim-*.tar.bz2
