35205a29f0f56adfcd4bed5f139c35c897a25045
Hoang Xuan Thanh <thanh.hx@zinza.com.vn>
2019-05-06 13:28:11 +0900

    Add some personal alias

--- bash/aliases/enable/docker-compose.aliases.bash
+++ bash/aliases/enable/docker-compose.aliases.bash
@@ -6,3 +6,8 @@ alias dcol='docker-compose logs -f --tail 100'
 alias dcoe='docker-compose exec'
 alias dcou='docker-compose up'
 alias dcos='docker-compose stop'
+
+# Fix personal type with git alias
+alias gcoe='dcoe'
+alias gcou='dcou'
+alias gcos='dcos'
--- bash/aliases/enable/tmux.aliases.bash
+++ bash/aliases/enable/tmux.aliases.bash
@@ -1,6 +1,7 @@
 cite 'about-alias'
 about-alias 'Tmux terminal multiplexer'
 
+alias tx='tmux'
 alias txl='tmux ls'
 alias txn='tmux new -s'
 alias txa='tmux a -t'
