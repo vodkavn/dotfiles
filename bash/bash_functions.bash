7aa2a9950bb5687d926362fbc4b056f7b913fd16
Hoang Xuan Thanh <thanh.hx@zinza.com.vn>
2019-05-08 02:29:08 +0900

    Add colors functions

--- bash/bash_functions.bash
+++ bash/bash_functions.bash
@@ -43,6 +43,34 @@ function load_bash_files() {
     fi
 }
 
+function load_colors_16() {
+    for fgbg in 38 48 ; do # Foreground / Background
+        for color in {0..15} ; do # Colors
+            # Display the color
+            printf "\e[${fgbg};5;%sm  %3s  \e[0m" $color $color
+            # Display 8 colors per lines
+            if [ $((($color + 1) % 8)) == 0 ] ; then
+                echo # New line
+            fi
+        done
+        echo # New line
+    done
+}
+
+function load_colors_256() {
+    for fgbg in 38 48 ; do # Foreground / Background
+        for color in {0..255} ; do # Colors
+            # Display the color
+            printf "\e[${fgbg};5;%sm  %3s  \e[0m" $color $color
+            # Display 6 colors per lines
+            if [ $((($color + 1) % 6)) == 4 ] ; then
+                echo # New line
+            fi
+        done
+        echo # New line
+    done
+}
+
 if ! type pathmunge > /dev/null 2>&1
 then
     function pathmunge () {
