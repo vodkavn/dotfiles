4d149004ff9f629ace1790992abb67bc298956c1
Hoang Xuan Thanh <hoangxuanthanh1986@gmail.com>
2023-05-15 02:17:18 +0900

    Change docker-compose aliases

--- bash/aliases/enable/docker-compose.aliases.bash
+++ bash/aliases/enable/docker-compose.aliases.bash
@@ -1,11 +1,11 @@
 cite 'about-alias'
-about-alias 'docker-compose abbreviations'
+about-alias 'docker compose abbreviations'
 
-alias dco='docker-compose'
-alias dcol='docker-compose logs -f --tail 100'
-alias dcoe='docker-compose exec'
-alias dcou='docker-compose up'
-alias dcos='docker-compose stop'
+alias dco='docker compose'
+alias dcol='docker compose logs -f --tail 100'
+alias dcoe='docker compose exec'
+alias dcou='docker compose up'
+alias dcos='docker compose stop'
 
 # Fix personal typo with git alias
 alias gcoe='dcoe'
